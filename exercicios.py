c5204f220e669e8b3d328336998b782653cefc67
Rafael Corte <rafcorte@gmail.com>
2026-01-22 11:14:50 -0300

    exercicio 3

--- exercicios.py
+++ exercicios.py
@@ -54,3 +54,20 @@ for i in temperaturas:
    
 
 # %%
+### Exercício 3: Filtragem de Logs por Severidade
+# Você está analisando logs de uma aplicação e precisa filtrar mensagens 
+# com severidade 'ERROR'. Dado um registro de log em formato de dicionário 
+# como `log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}`, 
+# escreva um programa que imprima a mensagem se a severidade for 'ERROR'.
+
+log = {'timestamp': '2021-06-23 10:00:00', 'level': 'ERROR', 'message': 'Falha na conexão'}
+
+valor = []
+
+for i in log.values():
+    valor.append(i)
+
+print(valor[2])
+
+# if log['level'] == 'ERROR':
+#     print(log['message'])
